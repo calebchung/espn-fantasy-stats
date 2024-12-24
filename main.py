92a838b63a14b348a9bda10b7cea542be68dab30
Caleb Chung <calebchung04@gmail.com>
2024-12-23 23:00:35 -0500

    rough draft impl of underperforming

--- main.py
+++ main.py
@@ -6,26 +6,47 @@ from selenium.webdriver.common.by import By
 
 from utils import clean_player_name
 
-base_url = "https://fantasy.espn.com/football/team?leagueId=883531153&teamId=1&seasonId=2024&scoringPeriodId=1&statSplit=singleScoringPeriod"
-options = webdriver.ChromeOptions()
-options.add_argument(argument="--headless")
-driver = webdriver.Chrome(options=options)
-driver.get(base_url)
+# teams and weeks are 1-indexed
+teams = range(1, 7)
+weeks = range(1, 2)
 
-def total_underperforming(driver):
-    WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.CLASS_NAME, "Table__Scroller")))
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
+def start_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument(argument="--headless")
+    driver = webdriver.Chrome(options=options)
+    return driver
 
-    for row in soup.find_all("tr", class_="Table__TR Table__TR--lg Table__odd")[:-2]:
-        try:
-            cols = row.find_all("td")
-            raw_name = cols[1].get_text()
-            if raw_name != "":
-                print(clean_player_name(raw_name))
-        except AttributeError:
-            continue
+def create_url(team_id: int = 1, week: int = 1):
+    url = ("https://fantasy.espn.com/football/team?leagueId=883531153&"
+           "teamId=" + str(team_id) +
+           "&seasonId=2024&"
+           "scoringPeriodId=" + str(week) + "&statSplit=singleScoringPeriod")
+    return url
 
 
+def total_underperforming():
+    driver = start_driver()
+    for team in teams:
+        for week in weeks:
+            url = create_url(team, week)
+            driver.get(url)
+            WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.CLASS_NAME, "Table__Scroller")))
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            total_diff_for_week = 0
+            for row in soup.find_all("tr", class_="Table__TR Table__TR--lg Table__odd")[:-9]: # exclude bench
+                cols = row.find_all("td")
+                if cols[1].get_text() == "":
+                    continue
+                name = clean_player_name(cols[1].get_text())
+                projected = float(cols[4].get_text())
+                if cols[5].get_text() == "--": # did not play
+                    actual = 0
+                else:
+                    actual = float(cols[5].get_text())
+                diff = round(actual - projected, 2)
+                print(name + " " + str(diff))
+                total_diff_for_week += diff
 
+            print("Week 1 diff vs projection: " + str(round(total_diff_for_week, 2)))
 
-total_underperforming(driver)
+total_underperforming()
