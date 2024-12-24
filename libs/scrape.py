8486f1fcb65a7de7fc49997b43dd4e4669a8d9d7
Caleb Chung <calebchung04@gmail.com>
2024-12-24 15:43:05 -0500

    first pass at putting into pd

--- libs/scrape.py
+++ libs/scrape.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 from libs.utils import create_url
 
+import pandas as pd
+
 # teams and weeks are 1-indexed
 teams = range(1, 7)
 weeks = range(1, 2)
@@ -19,13 +21,28 @@ def start_driver():
 # get it bc boil B)
 def boil_soups():
     driver = start_driver()
-    team_week_soups = {}
+    per_team = {}
     for team in teams:
+        week_soups = {}
         for week in weeks:
             url = create_url(team, week)
             driver.get(url)
             WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.CLASS_NAME, "Table__Scroller")))
             soup = BeautifulSoup(driver.page_source, 'html.parser')
-            team_week_soups[(team, week)] = soup
+            week_soups[week] = soup
+        per_team[team] = week_soups
 
-    return team_week_soups
+    return per_team
+
+def scrape():
+    team_soups = boil_soups()
+    team_dfs = {}
+    for week_idx in team_soups.keys():
+        week_dfs = {}
+        for week_soup_idx in team_soups[week_idx]:
+            week_table = team_soups[week_idx][week_soup_idx].find_all('table')
+            week_df = pd.read_html(str(week_table))[0]
+            week_dfs[week_soup_idx] = week_df
+
+        team_dfs[week_idx] = week_dfs
+    return team_dfs
--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-from libs.scrape import boil_soups
+from libs.scrape import boil_soups, scrape
 from libs.utils import clean_player_name
 
 def total_underperforming():
-    team_week_soups = boil_soups()
-    for soup in team_week_soups.values():
-        total_diff_for_week = 0
-        for row in soup.find_all("tr", class_="Table__TR Table__TR--lg Table__odd")[:-9]: # exclude bench
-            cols = row.find_all("td")
-            if cols[1].get_text() == "":
-                continue
-            name = clean_player_name(cols[1].get_text())
-            projected = float(cols[4].get_text())
-            if cols[5].get_text() == "--": # did not play
-                actual = 0
-            else:
-                actual = float(cols[5].get_text())
-            diff = round(actual - projected, 2)
-            print(name + " " + str(diff))
-            total_diff_for_week += diff
+    team_soups = boil_soups()
+    for week_list in team_soups.values():
+        for week_soup in week_list.values():
+            total_diff_for_week = 0
+            for row in week_soup.find_all("tr", class_="Table__TR Table__TR--lg Table__odd")[:-9]: # exclude bench
+                cols = row.find_all("td")
+                if cols[1].get_text() == "":
+                    continue
+                name = clean_player_name(cols[1].get_text())
+                projected = float(cols[4].get_text())
+                if cols[5].get_text() == "--": # did not play
+                    actual = 0
+                else:
+                    actual = float(cols[5].get_text())
+                diff = round(actual - projected, 2)
+                print(name + " " + str(diff))
+                total_diff_for_week += diff
 
-        print("Week 1 diff vs projection: " + str(round(total_diff_for_week, 2)))
+            print("Week 1 diff vs projection: " + str(round(total_diff_for_week, 2)))
 
-total_underperforming()
+# total_underperforming()
+df = scrape()
+print(df)
