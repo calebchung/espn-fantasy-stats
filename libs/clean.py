fc70170eaafcef25658cf6dac7a7170852fb1c18
Caleb Chung <calebchung04@gmail.com>
2024-12-24 21:13:37 -0500

    some cleaning

--- libs/clean.py
+++ libs/clean.py
@@ -1,5 +1,4 @@
-import pandas as pd
-import numpy as np
+from libs.utils import clean_player_name
 
 def clean(df_dict):
     for team_weeks in df_dict.values():
@@ -12,4 +11,11 @@ def clean(df_dict):
             week_df.drop('STATUS', axis=1, inplace=True)
             week_df.drop('LAST', axis=1, inplace=True)
             week_df.fillna(0, inplace=True)
+            week_df.replace({'--': 0}, regex=True, inplace=True)
+            week_df['Player'] = week_df['Player'].apply(lambda raw_name: clean_if_name(raw_name))
     return df_dict
+
+def clean_if_name(name_or_int):
+    if isinstance(name_or_int, str) and " " in name_or_int:
+        return clean_player_name(name_or_int)
+    return name_or_int
